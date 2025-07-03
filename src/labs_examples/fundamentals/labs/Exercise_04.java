1c108171a398b6cf8fcc459bb7ec7d7c5626ec4e
projectpk5 <pavan.narayanan@gmail.com>
2025-07-03 14:11:44 +0530

    Exercise_04.java

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -1,5 +1,7 @@
 package labs_examples.fundamentals.labs;
 
+import java.net.SocketOption;
+
 /**
  * Fundamentals Exercise 4: My Second Program
  *
@@ -16,7 +18,26 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        boolean a = true;
+        System.out.println("boolean a is: "+a);
 
+        byte smallValue = 78;
+        System.out.println("byte smallvalue is: "+smallValue);
+
+        char newChar = 'c';
+        System.out.println("char newChar is: "+newChar);
+
+        short shortInteger = 35;
+        System.out.println("short shortInteger is: "+shortInteger);
+
+        long longInteger = 357846358;
+        System.out.println("long longInteger is : " +longInteger);
+
+        float myFloatingPoint = 8745.33f;
+        System.out.println("float myFloatingPoint is : " +myFloatingPoint);
+
+        double myDouble = 394875.345;
+        System.out.println("double myDouble is : " +myDouble);
     }
 
 }
