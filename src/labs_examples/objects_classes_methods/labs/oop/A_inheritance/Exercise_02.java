d2e9e3ef98fd743690922e79130b0e1fca9e17d3
projectpk5 <pavan.narayanan@gmail.com>
2025-07-07 22:19:46 +0530

    completed remaining exercises in inheritance

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
@@ -3,8 +3,11 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 /**
  * Run the class below and study the output. What prints and why?
  *
- * Your answer:
- *
+ * Your answer: variable i in the class A prints here. We have declared class A's
+ * object "a" as "A a" and initialized to "new B()"
+ * B extends A which means subclass inherits parent class's variables and methods;
+ * Even though Class B has the variable i, declared object references the class A and hence it is
+ * printing 10.
  */
 class A {
     int i = 10;
@@ -15,7 +18,6 @@ class B extends A{
 }
 
 public class Exercise_02 {
-
     public static void main(String[] args) {
         A a = new B();
 
--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
@@ -4,7 +4,12 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
  * Why does the output print in the order it does?
  *
  * You answer:
- *
+ * Class A Constructor
+ * Class B Constructor
+ * Class C Constructor
+ * example of constructor of superclass getting called. the output works like as if it is a recursion problem
+ * in this case, we have superclass C calling B calling A and output starts printing from superclass then
+ * subclass. In this case, A followed by B followed by C.
  */
 
 class A_1 {
@@ -22,15 +27,9 @@ class B_1 extends A_1 {
 }
 
 class C_1 extends B_1 {
-    public C_1()
-    {
-        System.out.println("Class C Constructor");
-    }
+    public C_1() { System.out.println("Class C Constructor");}
 }
 
 public class Exercise_03 {
-    public static void main(String[] args)
-    {
-        C_1 c = new C_1();
-    }
+    public static void main(String[] args) { C_1 c = new C_1();}
 }
